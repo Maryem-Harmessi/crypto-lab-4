98702792246a6896d1f6d1567f0d737b6d6b9580
LENOVO <hermassimyriam@gmail.com>
2026-01-13 18:33:55 +0100

    export prekey bundle task 1

--- keys.py
+++ keys.py
@@ -1,8 +1,14 @@
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import ed25519
 from cryptography.exceptions import InvalidSignature
+import json
+import base64
 
-#generate iidentity key pair
+# Helpers
+def b64e(b: bytes) -> str:
+    return base64.b64encode(b).decode("utf-8")
+
+# Generate identity key pair
 ik_private = ed25519.Ed25519PrivateKey.generate()
 
 ik_public = ik_private.public_key()
@@ -26,10 +32,12 @@ spk_public_bytes = spk_public.public_bytes(
 
 spk_signature = ik_private.sign(spk_public_bytes)
 
-try:(
+try:
     ik_public.verify(spk_signature, spk_public_bytes)
-    )
-except:  print("signature failed")
+    print("SPK signature verified")
+except InvalidSignature:
+    print("SPK signature failed")
+    raise
 
 print("Signed prekey generated")
 
@@ -38,16 +46,28 @@ opk_num=4
 opk_private_keys = []
 opk_public_bytes_list = []
 
-for i in range(opk_num):
+for i in range(opk_num-1):
     opk_private = ed25519.Ed25519PrivateKey.generate()
     opk_public = opk_private.public_key()
-
     opk_public_bytes = opk_public.public_bytes(
         encoding=serialization.Encoding.Raw,
         format=serialization.PublicFormat.Raw
     )
-
-    opk_private_keys.append(opk_private)
     opk_public_bytes_list.append(opk_public_bytes)
+
+# Encoding and bundling the prekey bundle
+bundle = {
+        "IK_pub": b64e(ik_public_bytes),
+        "SPK_pub": b64e(spk_public_bytes),
+        "SPK_sig": b64e(spk_signature),
+        "OPK_pub_list": [b64e(x) for x in opk_public_bytes_list],
+    }
+
+# Export prekey bundle
+with open("prekey_bundle.json", "w", encoding="utf-8") as f:
+    json.dump(bundle, f, indent=2)
+
+opk_private_keys.append(opk_private)
+opk_public_bytes_list.append(opk_public_bytes)
 
 print(f"OPKs generated: {len(opk_public_bytes_list)}")
